a717e5d97f6c537a9faffa9bbf5de9574110324e
Endi Rayanna Nunes Costa Mandú <rayannamandu@academicos.com>
2024-03-05 19:39:30 -0300

    Array Aula

--- Aula3/src/Aluno.java
+++
@@ -1,8 +0,0 @@
-public class Aluno extends Pessoa{
-    String matricula;
-
-    public Aluno(String nome, String cpf, String data_nascimento, String matricula) {
-        super(nome, cpf, data_nascimento);
-        this.matricula = matricula;
-    }
-}
